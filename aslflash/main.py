664d3fe7e0281960a3c8d94c26a30f14f848bb7f
Stephen Polcyn <spolcyn@users.noreply.github.com>
2022-02-11 19:27:39 -0500

    Remove bash script comments

--- aslflash/main.py
+++ aslflash/main.py
@@ -49,24 +49,8 @@ def render():
         password_in.empty()
         try_login_in.empty()
 
-    #!/bin/bash
-    # https://stackoverflow.com/questions/7427262/how-to-read-a-file-into-a-variable-in-shell
-    # VIDEO_PATH=$1
-    # SEGMENTS=$(<$2)
-
-    # https://superuser.com/questions/692714/how-to-split-videos-with-ffmpeg-and-segment-times-option
-    # DIR="$VIDEO_PATH output"
-    # mkdir "$DIR"
-    # ffmpeg -i "$VIDEO_PATH" -f segment -segment_times $SEGMENTS \
-    # -c:v copy -map 0 -c:a copy \
-    # -reset_timestamps 1 "$DIR/$1-word%d.mov"
-    # Old part:
-    # ffmpeg -i "$VIDEO_PATH" -f segment -segment_times $SEGMENTS -c copy \
-    # -vcodec vp9 \
-    # -acodec libvorbis -map 0 \
-    # -reset_timestamps 1 "$DIR/$1-word%d.webm"
-
     # References:
+    # https://superuser.com/questions/692714/how-to-split-videos-with-ffmpeg-and-segment-times-option
     # Time unit syntax: https://trac.ffmpeg.org/wiki/Seeking#Timeunitsyntax
     # Reset timestamps to avoid all videos black but the
     # first: https://trac.ffmpeg.org/ticket/142://trac.ffmpeg.org/ticket/1425
