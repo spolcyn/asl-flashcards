320caf81d140e077c7666b8a479c2e0ec70c3ef3
Stephen Polcyn <spolcyn@users.noreply.github.com>
2023-09-24 14:29:59 -0400

    Remove unnecessary cast

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 with open("VERSION") as f:
-    version = float(f.read())
+    version = f.read()
 
 setup(
     name="aslflash",
-    version=str(version),
+    version=version,
     description="ASL Flashcard Builder",
     author="Stephen Polcyn",
     url="spyn.us",
