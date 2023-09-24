9f29cb85b1e9e63bd6ff94f78f44dd5b4b72bb56
Stephen Polcyn <spolcyn@users.noreply.github.com>
2023-09-24 14:07:16 -0400

    Add type hints

--- aslflash/main.py
+++ aslflash/main.py
@@ -2,6 +2,7 @@ from io import BytesIO
 import logging
 from pathlib import Path
 from tempfile import NamedTemporaryFile
+from typing import Optional
 
 import pandas as pd
 import streamlit as st
@@ -18,11 +19,11 @@ from aslflash.utils import (
     rename_videos,
 )
 
-logger = logging.getLogger(__name__)
+logger: logging.Logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 
-def render():
+def render() -> None:
     # References:
     # https://superuser.com/questions/692714/how-to-split-videos-with-ffmpeg-and-segment-times-option
     # Time unit syntax: https://trac.ffmpeg.org/wiki/Seeking#Timeunitsyntax
@@ -33,7 +34,7 @@ def render():
 
     st.header("Vocab and Timing List Upload")
 
-    choice = st.selectbox(
+    choice: str = st.selectbox(
         "What do you want to do?", ["Make flashcards", "View documentation"]
     )
 
@@ -45,22 +46,22 @@ def render():
         assert False
 
 
-def render_docs():
-    doc_path = Path(__file__).parent.parent / "docs" / "docs.md"
+def render_docs() -> None:
+    doc_path: Path = Path(__file__).parent.parent / "docs" / "docs.md"
     with open(doc_path, mode="r") as doc_file:
-        doc_body = doc_file.read()
+        doc_body: str = doc_file.read()
 
     st.markdown(doc_body, unsafe_allow_html=True)
 
 
-def render_app():
-    vocab_timing_csv = st.file_uploader(
+def render_app() -> None:
+    vocab_timing_csv: Optional[BytesIO] = st.file_uploader(
         "CSV file with Vocab and Timing info", type=["csv"]
     )
-    segment_string = None
-    vocab_timing_df = None
+    segment_string: Optional[str] = None
+    vocab_timing_df: Optional[pd.DataFrame] = None
     if vocab_timing_csv:
-        vocab_timing_data = BytesIO(vocab_timing_csv.getvalue())
+        vocab_timing_data: BytesIO = BytesIO(vocab_timing_csv.getvalue())
         try:
             vocab_timing_df = get_vocab_timing_df(vocab_timing_data)
             validate_word_timing_df(vocab_timing_df)
@@ -76,13 +77,13 @@ def render_app():
 
     st.header("Video Upload")
 
-    finished_computation = False
-    split_video_dir = None
-    video_split_dependencies = [vocab_timing_df is not None, segment_string]
+    finished_computation: bool = False
+    split_video_dir: Optional[str] = None
+    video_split_dependencies: list[Optional[bool]] = [vocab_timing_df is not None, segment_string]
     if all(video_split_dependencies):
-        video_file = st.file_uploader("Video with words signed")
+        video_file: Optional[BytesIO] = st.file_uploader("Video with words signed")
         if video_file:
-            video_data = video_file.getvalue()
+            video_data: bytes = video_file.getvalue()
             # TODO(spolcyn): Verify timestamps and video length are compatible
             # (i.e., largest time stamp < length of video)
             split_video_dir = split_video(video_data, segment_string)  # type: ignore
@@ -98,18 +99,18 @@ def render_app():
 
     st.header("Split Video Download")
 
-    words = None
-    video_paths = None
-    zip_videos_dependencies = [finished_computation, split_video_dir]
+    words: Optional[pd.Series] = None
+    video_paths: Optional[list[str]] = None
+    zip_videos_dependencies: list[Optional[bool]] = [finished_computation, split_video_dir]
     if all(zip_videos_dependencies):
-        zip_output_path = zip_dir(split_video_dir)  # type: ignore
+        zip_output_path: str = zip_dir(split_video_dir)  # type: ignore
 
         # Create a CSV file to import to Anki
-        VIDEO_OUTPUT_EXTENSION = "mp4"
+        VIDEO_OUTPUT_EXTENSION: str = "mp4"
         words = vocab_timing_df["word"]
         video_paths = [f"[sound:{word}.{VIDEO_OUTPUT_EXTENSION}]" for word in words]
 
-    anki_import_dependencies = [words is not None, video_paths is not None]
+    anki_import_dependencies: list[Optional[bool]] = [words is not None, video_paths is not None]
     if all(anki_import_dependencies):
         tags = st_tags(
             label="Anki tags for flashcards",
@@ -118,14 +119,14 @@ def render_app():
             maxtags=-1,
         )
 
-        done_with_tags = st.button("Finish adding tags and generate flashcard deck")
+        done_with_tags: bool = st.button("Finish adding tags and generate flashcard deck")
         if not done_with_tags:
             return
 
-        card_tags = [tags for _ in range(len(words))]
-        df_dict = {"word": words, "video_path": video_paths, "tags": card_tags}
-        anki_import_df = pd.DataFrame.from_dict(df_dict)
-        anki_import_csv = bytes(
+        card_tags: list[str] = [tags for _ in range(len(words))]
+        df_dict: dict[str, list[str]] = {"word": words, "video_path": video_paths, "tags": card_tags}
+        anki_import_df: pd.DataFrame = pd.DataFrame.from_dict(df_dict)
+        anki_import_csv: bytes = bytes(
             anki_import_df.to_csv(line_terminator="\n", header=False, index=False),
             encoding="utf-8",
         )
